0f85044142c252fbcd8efde6ef4637e0596dcfe3
bear7066 <kidflash7011>
2026-05-29 23:50:00 +0800

    [Feat] finish finetuning and inferencing, separate 2 commit to push to git

--- project/settings.py
+++ project/settings.py
@@ -45,11 +45,11 @@ INFERENCE_RESULT_DIR = Path("result/inference_images")
 
 # Finetuning Noise2NoiseUNet ----------------------------------------------------
 FT_WANDB_PROJECT = "ip-final-project-p2n"
-FT_WANDB_RUN = "SSL_ver1"  # Change this for each finetuning run
+FT_WANDB_RUN = "SSL-zero-one-003"  # Change this for each finetuning run
 FT_RESULT_DIR = Path("result/finetune_images")
 
 NUM_ITERATION = 100
-FT_CROP_SIZE = 2048
+FT_CROP_SIZE = 1024
 
 SIGMA = 0.75
 
